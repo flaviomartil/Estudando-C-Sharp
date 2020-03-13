3be35f220965b493d69340df15e1cefa4c9a7a80
Flavio Martil <flaviomartil5@gmail.com>
2020-03-13 11:56:42 -0300

    Interpolação aula - 19

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -11,6 +11,7 @@ namespace CursoCSharp {
                 {"Comentarios  - Fundamentos", Comentarios.Executar},
                 {"Variaveis e Constantes  - Fundamentos", VariaveisEConstantes.Executar},
                 {"Infêrencia  - Fundamentos", Infêrencia.Executar},
+                {"Interpolação  - Fundamentos", Interpolação.Executar},
             });
 
             central.SelecionarEExecutar();
