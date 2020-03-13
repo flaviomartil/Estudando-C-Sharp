30266f8b8c497fdd359390f67aca45256527abf5
Flavio Martil <flaviomartil5@gmail.com>
2020-03-13 11:36:38 -0300

    Infêrencias Aula - 18

---
+++ CursoCSharp/Fundamentos/Infêrencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Infêrencia {
+        public static void Executar() {
+            var nome = "Leonardo";
+            Console.WriteLine(nome);
+            var idade = 15;
+            Console.WriteLine(idade);
+
+            /*Não posso fazer var idade;
+             * e idade = 15;
+             * Pois ele não saberá converter porque não temos nenhum dado no var */
+
+            //Declarei variavel
+            int a;
+            a = 15;
+            //Declarei e inicializei a variavel
+            int b = 10;
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
