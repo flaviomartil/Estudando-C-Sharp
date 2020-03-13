bc3b9d531f60defdce4f8fb6ba74ecc9fea28bdf
Flavio Martil <flaviomartil5@gmail.com>
2020-03-13 09:47:17 -0300

    Aula 16 Comentarios de código

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+/// <summary>
+/// 
+/// </summary>
+    class Comentarios {
+
+        public static void Executar() {
+           
+            // cuidado com os comentarios desnecessários
+            Console.WriteLine("Código claro é sempre melhor");
+            Console.WriteLine("O C# tem um XML comments");
+            /*
+             * 
+             */
+            Console.WriteLine("O C# tem um XML comments");
+            Console.WriteLine("o c# tem um xml comments");
+        }
+    }
+}
