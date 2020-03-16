22b5d5f6d1d9025e5cc442d497688c91d5448f07
Flavio Martil <flaviomartil5@gmail.com>
2020-03-16 17:29:55 -0300

    Estrutura Switch - Aula 34

---
+++ CursoCSharp/EstruturasDeControle/EstruturaSwitch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaSwitch {
+        public static void Executar() {
+        
+        }
+    }
+}
