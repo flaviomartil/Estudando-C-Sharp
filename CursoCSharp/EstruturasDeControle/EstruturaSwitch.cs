bf154cc7ed13d396fbc4ef5dd762dbd4650449d9
flaviomartil <60192455+flaviomartil@users.noreply.github.com>
2020-03-16 17:30:41 -0300

    Update EstruturaSwitch.cs

--- CursoCSharp/EstruturasDeControle/EstruturaSwitch.cs
+++ CursoCSharp/EstruturasDeControle/EstruturaSwitch.cs
@@ -1,11 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CursoCSharp.EstruturasDeControle {
     class EstruturaSwitch {
         public static void Executar() {
-        
+            Console.WriteLine("Avalie meu atendimento com uma nota de 1 a 5");
+            int.TryParse(Console.ReadLine(), out int nota);
+            switch (nota) {
+                case 0:
+                    Console.WriteLine("O atendimento foi Péssimo");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("O atendimento foi Ruim");
+                    break;
+                case 3:
+                    Console.WriteLine("O atendimento foi Regular");
+                    break;
+                case 4:
+                    Console.WriteLine("O atendimento foi Bom");
+                    break;
+                case 5:
+                    Console.WriteLine("O atendimento foi Ótimo");
+                    break;
+                default:
+                    Console.WriteLine("Nota invalida,por favor digitar uma nota valida");
+                    break;
+            }
+            Console.WriteLine("Obrigado por responder");
         }
     }
 }
