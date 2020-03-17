8bbefb5e80d0a3cd08d5b5b37862eae0f54b2391
Flavio Martil <flaviomartil5@gmail.com>
2020-03-17 15:58:36 -0300

    Estrutura FOR Aula 37

---
+++ CursoCSharp/EstruturasDeControle/EstruturaFor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaFor {
+        public static void Executar() {
+            //for (int i = 1; i <= 10; i++) {
+            //    Console.WriteLine("O valor de i é: {0}", i);
+            //    i += 1;
+
+            //}
+
+            double somatorio = 0;
+            string entrada;
+
+            Console.Write("Informe o tamanho da turma: ");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int tamanhoDaTurma);
+
+            for (int i = 1; i <= tamanhoDaTurma; i++) {
+                Console.Write("Informe a nota do aluno {0}:  ", i);
+                entrada = Console.ReadLine();
+                double.TryParse(entrada, out double notaAtual);
+                somatorio += notaAtual;
+      
+            }
+            double media = tamanhoDaTurma > 0 ? somatorio / tamanhoDaTurma : 0;
+            Console.WriteLine("A média da turma é de: {0}",media);
+        }
+    }
+}
