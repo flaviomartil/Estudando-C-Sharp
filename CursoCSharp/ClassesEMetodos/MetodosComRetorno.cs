7747d47f7fa32d4f4785043342a0060a4ec62c52
Flavio Martil <flaviomartil5@gmail.com>
2020-03-20 09:56:24 -0300

    Métodos Com Retorno #01 e  #02 - Aulas 45,46

--- CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -17,6 +17,30 @@ class CalculadoraComum {
             return a / b;
         }
     }
+    public class CalculadoraCadeia {
+        int memoria;
+        public CalculadoraCadeia Somar(int a) {
+
+            memoria += a;
+            return this;
+        }
+         public  CalculadoraCadeia Multiplicar(int a) {
+            memoria *= a;
+            return this;
+        }
+        public CalculadoraCadeia Limpar() {
+            memoria = 0;
+            return this;
+        }
+    public CalculadoraCadeia Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+        public int Resultado() {
+            return memoria;
+        }
+    }
+
     class MetodosComRetorno {
         public static void Executar() {
             var calculadoraComum = new CalculadoraComum();
@@ -25,6 +49,12 @@ class CalculadoraComum {
             Console.WriteLine(calculadoraComum.Subtrair(2,2));
             Console.WriteLine(calculadoraComum.Multiplicar(9,9));
             Console.WriteLine(calculadoraComum.Divisao(45,5));
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+
+            resultado = calculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+            Console.WriteLine(resultado);
 
         }
     }
