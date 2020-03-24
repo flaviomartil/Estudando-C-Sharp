522d6caf6f25c6fd4ae70f0108dc46f64eb9e911
Flavio Martil <flaviomartil5@gmail.com>
2020-03-24 15:25:14 -0300

    Correções comentario do código aula 58

--- CursoCSharp/ClassesEMetodos/StructVsClasse.cs
+++ CursoCSharp/ClassesEMetodos/StructVsClasse.cs
@@ -18,14 +18,14 @@ namespace CursoCSharp.ClassesEMetodos {
             SPonto copiaSPonto = ponto1;
             ponto1.X = 10;
             Console.WriteLine("O valor do ponto  é {0}",ponto1.X); // É 10 FOI INSTANCIADO
-            Console.WriteLine("O valor do ponto  é {0}",copiaSPonto.X); // É 1 O STRUCT RESPEITA A Atribuição
+            Console.WriteLine("O valor do ponto  é {0}",copiaSPonto.X); // Atribuição por valor
 
             CPonto cponto1 = new CPonto { X = 2, Y =  20 };
             CPonto copiaCPonto1 = cponto1;
             cponto1.X = 22;
             Console.WriteLine("O valor do ponto 2 é {0}",cponto1.X);
             Console.WriteLine("O valor do ponto 2 é {0}",copiaCPonto1.X);
-            //Ambos serão Iguais devido a classe referenciar
+            //Atribuição por refêrencia
         }
     }
 }
