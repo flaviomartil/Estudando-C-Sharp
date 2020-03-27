122b79df8239d32700a1df41d6ddac2060b59ce0
Flavio Martil <flaviomartil5@gmail.com>
2020-03-27 16:56:49 -0300

    Stack(LIFO) Aula 68

---
+++ CursoCSharp/Colecoes/ColecoesStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace CursoCSharp.Colecoes {
+    class ColecoesStack {
+        public static void Executar() {
+            var pilha = new Stack();
+
+            pilha.Push(3);
+            pilha.Push("a");
+            pilha.Push(true);
+            pilha.Push(3.14);
+
+            foreach(var item in pilha) {
+                Console.WriteLine("Listando "+ item +" " );
+            }
+            Console.WriteLine(pilha.Count);
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Peek());
+            Console.WriteLine(pilha.Count);
+        }
+    }
+}
